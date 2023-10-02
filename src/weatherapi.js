b1c3b49069000d6dd7e550d8a74a11a95cf2e6b1
unknown <joshuel.simbulan@gmail.com>
2023-10-02 20:03:45 +0800

    add more styling

--- src/weatherapi.js
+++ src/weatherapi.js
@@ -1,15 +1,23 @@
-class Weather {
+export default class Weather {
     static key = '3941436912164c0b9d120339233009';
-    constructor() {
+    static default_location = "Philadelphia"
 
+    constructor(location) {
+        this.location = location || Weather.default_location
     }
-
-    static getWeatherInfo() {
-        return fetch(`http://api.weatherapi.com/v1/current.json?key=${key}`)
-        .then(response => {
-            return response
-        }).catch(err => {
-            return err
-        })
-    }
+  
+    async getWeatherInfo(location) {
+        const response = await fetch(
+          `http://api.weatherapi.com/v1/forecast.json?key=${Weather.key}&q=${this.location}&days=3`,
+          {
+            mode: 'cors',
+          }
+        );
+        if (response.status === 400) {
+          throwErrorMsg();
+        } else {
+          const weatherInfo = await response.json();
+          console.log(weatherInfo)
+        }
+      }
 }
